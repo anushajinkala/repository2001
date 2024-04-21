3b1a666323f166119e8130d7bc682fe4f1214db9
anu <jinkalaanusha38@gmail.com>
2024-04-21 08:46:28 +0530

    adding img

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
     <h1>Elon Musk was scheduled to meet Prime Minister Narendra Modi during his trip to India.</h1>
     <p>Elon Musk, Tesla chief and one of the world's richest people, has postponed his much-awaited trip to India. The SpaceX founder and CEO was scheduled to meet Prime Minister Narendra Modi in New Delhi during his visit. The billionaire blamed “very heavy Tesla obligations” for his decision.</p>
-
-
+    <img src="./images/pic.jpeg"></img>
     </div>
   );
 }
